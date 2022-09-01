95fb2495a018faf3bd5e43b4b5e0cdaa252a3704
masa <dayoujianghong@gmail.com>
2022-09-01 10:53:17 +0900

    first commit

---
+++ public/dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+
+console.log('hello');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
