c211f0d96f6a44f39fba7e29cfde7ce06047880a
masa <dayoujianghong@gmail.com>
2022-09-02 12:14:22 +0900

    edit server/app.ts -> edit file path

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '../../../public'));
 
 // Serve your app
 console.log('Served: http://localhost:' + port);
--- server/dist/app.js
+++ server/dist/app.js
@@ -1,14 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// const express = require('express');
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 8080;
 // Serve static files
-app.use(express_1.default.static(__dirname + '/public'));
+app.use(express.static(__dirname + '../../../public'));
 // Serve your app
 console.log('Served: http://localhost:' + port);
 app.listen(port);
