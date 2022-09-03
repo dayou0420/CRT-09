095bcfbef6a671a0d337447df28026ee0afba299
masa <dayoujianghong@gmail.com>
2022-09-03 09:18:15 +0900

    edit src/food-app.ts -> 122

--- src/food-app.ts
+++ src/food-app.ts
@@ -10,6 +10,7 @@ class Food {
 class Foods {
     elements = document.querySelectorAll<HTMLDivElement>('.food');
     private _activeElements: HTMLDivElement[] = [];
+    private _activeElementsScore: number[] = [];
     get activeElements() {
         this._activeElements = [];
         this.elements.forEach(element => {
@@ -19,6 +20,16 @@ class Foods {
         });
         return this._activeElements;
     }
+    get activeElementsScore() {
+        this._activeElementsScore = [];
+        this.activeElements.forEach(element => {
+            const foodScore = element.querySelector('.food__score');
+            if (foodScore) {
+                this._activeElementsScore.push(Number(foodScore.textContent));
+            }
+        });
+        return this._activeElementsScore;
+    }
     constructor() {
         this.elements.forEach(element => {
             new Food(element);
@@ -26,4 +37,3 @@ class Foods {
     }
 }
 const foods = new Foods();
-console.log(foods.activeElements);
