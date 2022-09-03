f7671330a3214430682a67516d1073b1373ae03d
masa <dayoujianghong@gmail.com>
2022-09-03 10:56:12 +0900

    add food

--- server/dist/app.js
+++ server/dist/app.js
@@ -4,6 +4,10 @@ const app = express();
 const port = process.env.PORT || 8080;
 // Serve static files
 app.use(express.static(__dirname + '../../../public'));
+// Serve precipitation food app folder
+app.get('/food', (_, res) => {
+    res.sendFile(__dirname + '../../../public/food/index.html');
+});
 // Serve your app
 console.log('Served: http://localhost:' + port);
 app.listen(port);
