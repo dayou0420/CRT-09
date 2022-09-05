d94da9c31157d84f737b3a8778247bba1b60066d
masa <dayoujianghong@gmail.com>
2022-09-05 16:36:02 +0900

    edit src/app.ts -> 27

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -20,6 +20,13 @@ combineValues = add;
 // combineValues = printResult;
 console.log(combineValues(8, 8));
 printResult(add(5, 12));
+function addAndHandle(n1, n2, cb) {
+    const result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, result => {
+    console.log(result);
+});
 /***
  * 21, 22
 */
--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,13 @@ combineValues = add;
 // combineValues = printResult;
 console.log(combineValues(8, 8));
 printResult(add(5, 12));
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, result => {
+    console.log(result);
+});
 /***
  * 23
 */
