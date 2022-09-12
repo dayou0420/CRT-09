7b17f5f9f9082d63fbf9b4b79e841e0974195a31
masa <dayoujianghong@gmail.com>
2022-09-12 16:15:01 +0900

    edit src/app.ts -> 84

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -10,6 +10,45 @@ const e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('StartDate: ' + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+class Car {
+    drive() {
+        console.log('運転中...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('トラックを運転中...');
+    }
+    loadCargo(amount) {
+        console.log('荷物を載せています ...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 
 /******/ })()
 ;
