a50d2380457e9278ee2045b15a3f398e381bd22d
masa <dayoujianghong@gmail.com>
2022-09-18 08:40:50 +0900

    edit src/app.ts -> 97

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -25,7 +25,15 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj);
+function countAndDescribe(element) {
+    let descriptionText = '値がありません。';
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です。';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(['Sports', 'Cooking']));
 
 /******/ })()
 ;
--- src/app.ts
+++ src/app.ts
@@ -18,4 +18,18 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj);
+/***
+ * 97
+*/
+interface lengthy {
+    length: number;
+}
+function countAndDescribe<T extends lengthy>(element: T): [T, string] {
+    let descriptionText = '値がありません。';
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です。';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(['Sports', 'Cooking']));
