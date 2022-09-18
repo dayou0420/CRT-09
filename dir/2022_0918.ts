6028a45c42b5752da2513fe47b3763eeb0e1a8ae
masa <dayoujianghong@gmail.com>
2022-09-18 11:20:36 +0900

    edit src/app.ts -> 105

---
+++ dir/2022_0918.ts
@@ -0,0 +1,92 @@
+/***
+ * 94
+*/
+// const names: Array<string> = []; // string[]
+// names[0].split(' ');
+const promise = new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+        resolve('終わりました！');
+    }, 2000);
+});
+promise.then(data => {
+    data.split(' ');
+});
+/***
+ * 95, 96
+*/
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+// console.log(mergedObj);
+/***
+ * 97
+*/
+interface lengthy {
+    length: number;
+}
+function countAndDescribe<T extends lengthy>(element: T): [T, string] {
+    let descriptionText = '値がありません。';
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です。';
+    }
+    return [element, descriptionText];
+}
+// console.log(countAndDescribe(['Sports', 'Cooking']));
+/***
+ * 98
+*/
+function extractAndConvert<T extends Object, U extends keyof T>(obj: T, key: U) {
+    return 'Value: ' + obj[key];
+}
+extractAndConvert({ name: 'Max' }, 'name');
+/***
+ * 99, 102
+*/
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+    addItem(item: T) {
+        this.data.push(item);
+    }
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1); // -1
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Data1');
+textStorage.addItem('Data2');
+textStorage.removeItem('Data1');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage<number>();
+// const objStorage = new DataStorage<object>();
+// const obj = { name: 'Max' };
+// objStorage.addItem(obj);
+// objStorage.addItem({ name: 'Manu' });
+// objStorage.removeItem(obj);
+// console.log(objStorage.getItems());
+/***
+ * 101
+*/
+interface CourceGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+function createCourceGoal(
+    title: string, description: string, date: Date
+): CourceGoal {
+    let courceGoal: Partial<CourceGoal> = {};
+    courceGoal.title = title;
+    courceGoal.description = description;
+    courceGoal.completeUntil = date;
+    return courceGoal as CourceGoal;
+}
+const names: Readonly<string[]> = ['Max', 'Annna'];
+// names.push('Manu');
+// names.pop();
--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -1,82 +1,51 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.ts":
 /*!********************!*\
   !*** ./src/app.ts ***!
   \********************/
+/***/ (function() {
 
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 /***
- * 94
+ * 105
 */
-// const names: Array<string> = []; // string[]
-// names[0].split(' ');
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('終わりました！');
-    }, 2000);
-});
-promise.then(data => {
-    data.split(' ');
-});
-/***
- * 95, 96
-*/
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
+function Logger(constructor) {
+    console.log('ログ出力中...');
+    console.log(constructor);
 }
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-function countAndDescribe(element) {
-    let descriptionText = '値がありません。';
-    if (element.length > 0) {
-        descriptionText = '値は' + element.length + '個です。';
-    }
-    return [element, descriptionText];
-}
-// console.log(countAndDescribe(['Sports', 'Cooking']));
-/***
- * 98
-*/
-function extractAndConvert(obj, key) {
-    return 'Value: ' + obj[key];
-}
-extractAndConvert({ name: 'Max' }, 'name');
-/***
- * 99, 102
-*/
-class DataStorage {
+let Person = class Person {
     constructor() {
-        this.data = [];
+        this.name = 'Max';
+        console.log('Personオブジェクトを作成中...');
     }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1); // -1
-    }
-    getItems() {
-        return [...this.data];
-    }
-}
-const textStorage = new DataStorage();
-textStorage.addItem('Data1');
-textStorage.addItem('Data2');
-textStorage.removeItem('Data1');
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-function createCourceGoal(title, description, date) {
-    let courceGoal = {};
-    courceGoal.title = title;
-    courceGoal.description = description;
-    courceGoal.completeUntil = date;
-    return courceGoal;
-}
-const names = ['Max', 'Annna'];
-// names.push('Manu');
-// names.pop();
+};
+Person = __decorate([
+    Logger
+], Person);
+const pers = new Person();
+console.log(pers);
 
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.ts"]();
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
