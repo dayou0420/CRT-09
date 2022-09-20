d789195ed5d2271a105c4abe4b67bbdbb399ee56
masa <dayoujianghong@gmail.com>
2022-09-20 10:55:24 +0900

    edit src/app.ts -> 110

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -15,6 +15,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 /***
  * 105, 106
 */
@@ -63,11 +66,33 @@ function Log(target, propertyName) {
     console.log('Property デコレータ');
     console.log(target, propertyName);
 }
+/***
+ * 110
+*/
+function Log2(target, name, descriptor) {
+    console.log('Accessor デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('Method デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('Parameter デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 class Product {
     constructor(t, p) {
         this.title = t;
         this._price = p;
     }
+    // @Log2
     set price(val) {
         if (val > 0) {
             this._price = val;
@@ -76,13 +101,14 @@ class Product {
             throw new Error('不正な価格です - 0以下は設定できません');
         }
     }
+    // @Log3
     getPriceWithTax(tax) {
         return this._price * (1 + tax);
     }
 }
 __decorate([
-    Log
-], Product.prototype, "title", void 0);
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
 
 
 /***/ })
--- src/app.ts
+++ src/app.ts
@@ -44,10 +44,32 @@ function Log(target: any, propertyName: string | Symbol) {
     console.log('Property デコレータ');
     console.log(target, propertyName);
 }
+/***
+ * 110
+*/
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter デコレータ');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 class Product {
-    @Log
+    // @Log
     title: string;
     private _price: number;
+    // @Log2
     set price(val: number) {
         if (val > 0) {
             this._price = val;
@@ -59,7 +81,8 @@ class Product {
         this.title = t;
         this._price = p;
     }
-    getPriceWithTax(tax: number) {
+    // @Log3
+    getPriceWithTax(@Log4 tax: number) {
         return this._price * (1 + tax);
     }
 }
