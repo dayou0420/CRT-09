f9d56c3fe81acde1393b069c56d0aff91e124864
masa <dayoujianghong@gmail.com>
2022-09-22 15:50:30 +0900

    edit src/app.ts -> 127

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -64,6 +64,30 @@ function validate(validatableInput) {
     }
     return isValid;
 }
+/***
+ * 127
+*/
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type === 'active' ? '実行中プロジェクト' : '完了プロジェクト';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -131,6 +155,8 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
 class WeatherClient {
     constructor(apiKey) {
         this.apiKey = apiKey;
