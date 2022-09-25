2fae87bab1609fb2db366f57b9f12cc333fbeb64
masa <dayoujianghong@gmail.com>
2022-09-25 16:23:57 +0900

    add weather

---
+++ public/weather/dist/bundle.js
@@ -0,0 +1,464 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.ts":
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+/***/ (function() {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var ProjectStatus;
+(function (ProjectStatus) {
+    ProjectStatus[ProjectStatus["Active"] = 0] = "Active";
+    ProjectStatus[ProjectStatus["Finished"] = 1] = "Finished";
+})(ProjectStatus || (ProjectStatus = {}));
+class Project {
+    constructor(id, title, description, manday, status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.manday = manday;
+        this.status = status;
+    }
+}
+class State {
+    constructor() {
+        this.listeners = [];
+    }
+    addListener(listenerFn) {
+        this.listeners.push(listenerFn);
+    }
+}
+/***
+ * 137
+*/
+class ProjectState extends State {
+    constructor() {
+        super();
+        this.projects = [];
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
+    addProject(title, description, manday) {
+        const newProject = new Project(Math.random().toString(), title, description, manday, ProjectStatus.Active);
+        this.projects.push(newProject);
+        this.updateListeners();
+    }
+    moveProject(projectId, newStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+    updateListeners() {
+        for (const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice());
+        }
+    }
+}
+const projectState = ProjectState.getInstance();
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+function validate(validatableInput) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid =
+            isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid =
+            isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null &&
+        typeof validatableInput.value === 'number') {
+        isValid =
+            isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null &&
+        typeof validatableInput.value === 'number') {
+        isValid =
+            isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+/***
+ * 127, 131
+*/
+class Component {
+    constructor(templatedId, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templatedId);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.attach(insertAtStart);
+    }
+    attach(insertAtBeginning) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
+    }
+}
+/***
+ * 132, 133, 134, 136
+*/
+class ProjectItem extends Component {
+    constructor(hostId, project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+    get manday() {
+        if (this.project.manday < 20) {
+            return this.project.manday.toString() + '人日';
+        }
+        else {
+            return (this.project.manday / 20).toString() + '人月';
+        }
+    }
+    dragStartHandler(event) {
+        event.dataTransfer.setData('text/plain', this.project.id);
+        event.dataTransfer.effectAllowed = 'move';
+    }
+    dragEndHandler(_) {
+        console.log('Drag終了');
+    }
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+    }
+    renderContent() {
+        this.element.querySelector('h2').textContent = this.project.title;
+        this.element.querySelector('h3').textContent = this.manday;
+        this.element.querySelector('p').textContent = this.project.description;
+    }
+}
+__decorate([
+    autobind
+], ProjectItem.prototype, "dragStartHandler", null);
+/***
+ * 135, 136, 137
+*/
+class ProjectList extends Component {
+    constructor(type) {
+        super('project-list', 'app', false, `${type}-projects`);
+        this.type = type;
+        this.assignedProjects = [];
+        this.configure();
+        this.renderContent();
+    }
+    dragOverHandler(event) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul');
+            listEl.classList.add('droppable');
+        }
+    }
+    dropHandler(event) {
+        const prjId = event.dataTransfer.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
+    }
+    dragLeaveHandler(_) {
+        const listEl = this.element.querySelector('ul');
+        listEl.classList.remove('droppable');
+    }
+    configure() {
+        this.element.addEventListener('dragover', this.dragOverHandler);
+        this.element.addEventListener('drop', this.dropHandler);
+        this.element.addEventListener('dragleave', this.dragLeaveHandler);
+        projectState.addListener((projects) => {
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevantProjects;
+            this.renderProjects();
+        });
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type === 'active' ? '実行中プロジェクト' : '完了プロジェクト';
+    }
+    renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`);
+        listEl.innerHTML = '';
+        for (const prjItem of this.assignedProjects) {
+            new ProjectItem(listEl.id, prjItem);
+        }
+    }
+}
+__decorate([
+    autobind
+], ProjectList.prototype, "dragOverHandler", null);
+__decorate([
+    autobind
+], ProjectList.prototype, "dropHandler", null);
+__decorate([
+    autobind
+], ProjectList.prototype, "dragLeaveHandler", null);
+class ProjectInput extends Component {
+    constructor() {
+        super('project-input', 'app', true, 'user-input');
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.mandayInputElement = this.element.querySelector('#manday');
+        this.configure();
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    renderContent() {
+    }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredManday = this.mandayInputElement.value;
+        const titleValidatable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descriptionValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        const mandayValidatable = {
+            value: +enteredManday,
+            required: true,
+            min: 1,
+            max: 1000
+        };
+        if (!validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(mandayValidatable)) {
+            alert('入力値が正しくありません。再度お試しください。');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredManday];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.mandayInputElement.value = '';
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, manday] = userInput;
+            projectState.addProject(title, desc, manday);
+            console.log(title, desc, manday);
+            this.clearInputs();
+        }
+    }
+}
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
+const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
+class WeatherClient {
+    constructor(apiKey, cityName) {
+        this.apiKey = apiKey;
+        this.cityName = cityName;
+        this.getGeocoding(this.cityName);
+        this.getData();
+    }
+    getGeocoding(city) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const body = yield fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${this.apiKey}`);
+            const data = yield body.json();
+            return {
+                lat: data[0].lat,
+                lon: data[0].lon
+            };
+        });
+    }
+    getDailyWeather(latitude, longitude) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const body = yield fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.apiKey}`);
+            const data = yield body.json();
+            return {
+                city: data.name,
+                main: data.weather[0].main,
+                description: data.weather[0].description,
+                temp: data.main.temp,
+                humidity: data.main.humidity,
+                speed: data.wind.speed
+            };
+        });
+    }
+    getWeatherForecast(latitude, longitude) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const body = yield fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.apiKey}`);
+            const data = yield body.json();
+            const time = data.list.map((m) => m.dt_txt);
+            const temp = data.list.map((m) => m.main.temp);
+            const temp_max = data.list.map((m) => m.main.temp_max);
+            const temp_min = data.list.map((m) => m.main.temp_min);
+            const feels_like = data.list.map((m) => m.main.feels_like);
+            const humidity = data.list.map((m) => m.main.humidity);
+            const deg = data.list.map((m) => m.wind.deg);
+            const gust = data.list.map((m) => m.wind.gust);
+            const speed = data.list.map((m) => m.wind.speed);
+            const daily = {
+                labels: time,
+                datasets: [
+                    {
+                        label: 'Temperature',
+                        data: temp,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.2
+                    },
+                    {
+                        label: 'Temp Max',
+                        data: temp_max,
+                        borderColor: 'rgb(255, 99, 132)',
+                        tension: 0.2
+                    },
+                    {
+                        label: 'Temp Min',
+                        data: temp_min,
+                        borderColor: 'rgb(54, 162, 235)',
+                        tension: 0.2
+                    },
+                    {
+                        label: 'Feels Like',
+                        data: feels_like,
+                        borderColor: 'rgb(255, 159, 64)',
+                        tension: 0.2
+                    },
+                    {
+                        label: 'Humidity',
+                        data: humidity,
+                        borderColor: 'rgb(153, 102, 255)',
+                        tension: 0.2
+                    }
+                ]
+            };
+            new Chart(document.getElementById('daily'), {
+                type: 'line',
+                data: daily
+            });
+            const wind = {
+                labels: time,
+                datasets: [
+                    {
+                        label: 'Deg',
+                        data: deg,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.2
+                    },
+                    {
+                        label: 'Gust',
+                        data: gust,
+                        borderColor: 'rgb(255, 99, 132)',
+                        tension: 0.2
+                    },
+                    {
+                        label: 'Speed',
+                        data: speed,
+                        borderColor: 'rgb(54, 162, 235)',
+                        tension: 0.2
+                    },
+                ]
+            };
+            new Chart(document.getElementById('wind'), {
+                type: 'line',
+                data: wind
+            });
+        });
+    }
+    getData() {
+        this.getGeocoding(this.cityName)
+            .then(data => {
+            this.getDailyWeather(data.lat, data.lon)
+                .then(d => {
+                const city = document.getElementById('city');
+                const main = document.getElementById('main');
+                const des = document.getElementById('des');
+                const temp = document.getElementById('temp');
+                const humidity = document.getElementById('humidity');
+                const speed = document.getElementById('speed');
+                city.innerText = d.city;
+                main.innerText = d.main;
+                des.innerText = d.description;
+                temp.innerText = String(d.temp);
+                humidity.innerText = String(d.humidity);
+                speed.innerText = String(d.speed);
+            })
+                .catch(e => {
+                throw new Error(e.message);
+            });
+            this.getWeatherForecast(data.lat, data.lon);
+        })
+            .catch(err => {
+            throw new Error(err.message);
+        });
+    }
+}
+const API_KEY = '219228b2383f8240a93b11492d102a52';
+new WeatherClient(API_KEY, 'Shinagawa');
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.ts"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
+console.log('Served: http://localhost:' + port);
+app.get('/food', (_, res) => {
+    res.sendFile(__dirname + '/public/food/index.html');
+});
+app.get('/weather', (_, res) => {
+    res.sendFile(__dirname + '/public/weather/index.html');
+});
+app.listen(port);
