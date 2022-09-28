e7135a7b4dd6d2a1a1690d761e8fd141f3fbe88e
masa <dayoujianghong@gmail.com>
2022-09-28 10:26:10 +0900

    edit ts/src/unknown.ts

--- ts/src/unknown.ts
+++ ts/src/unknown.ts
@@ -3,5 +3,6 @@ const kansu = (): number => 43;
 let numberAny: any = kansu();
 let numberUnknown: unknown = kansu();
 let sumAny = numberAny + 10;
-console.log(typeof numberUnknown);
-// let sumUnknown = numberUnknown + 10;
+if (typeof numberUnknown === 'number') {
+    let sumUnknown = numberUnknown + 10;
+}
