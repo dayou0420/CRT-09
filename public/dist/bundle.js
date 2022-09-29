50792e492d96155951d74e9d17bb1fbd8a6dd263
masa <dayoujianghong@gmail.com>
2022-09-29 17:00:50 +0900

    edit src/app.ts

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -9574,7 +9574,7 @@ setTimeout(() => {
     console.log('Subscription 2 starts');
     observable$.subscribe((value) => console.log('Subscription 2:', value));
 }, 1000);
-const data$ = (0, fetch_1.fromFetch)('https://webapi-695u.onrender.com/foods').pipe((0, rxjs_1.switchMap)(response => {
+const data$ = (0, fetch_1.fromFetch)('https://webapi-695u.onrender.com/weights').pipe((0, rxjs_1.switchMap)(response => {
     if (response.ok) {
         return response.json();
     }
@@ -9586,7 +9586,36 @@ const data$ = (0, fetch_1.fromFetch)('https://webapi-695u.onrender.com/foods').p
     return (0, rxjs_1.of)({ error: true, message: err.message });
 }));
 data$.subscribe({
-    next: result => console.log(result)
+    next: (result) => {
+        const date = result.map(m => m.date);
+        const weight = result.map(m => m.weight);
+        const body_mass_index = result.map(m => m.body_mass_index);
+        const body_fat_percentage = result.map(m => m.body_fat_percentage);
+        const weights = {
+            labels: date,
+            datasets: [
+                {
+                    label: 'Weight',
+                    data: weight,
+                    borderColor: 'rgb(75, 192, 192)'
+                },
+                {
+                    label: 'BMI',
+                    data: body_mass_index,
+                    borderColor: 'rgb(255, 99, 132)',
+                },
+                {
+                    label: 'Body Fat Percentage',
+                    data: body_fat_percentage,
+                    borderColor: 'rgb(54, 162, 235)',
+                }
+            ]
+        };
+        new Chart(document.getElementById('weight'), {
+            type: 'line',
+            data: weights
+        });
+    }
 });
 
 })();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Observable, switchMap, of, catchError } from 'rxjs'
 import { fromFetch } from 'rxjs/fetch';
+declare var Chart: any;
 const observable$ = new Observable<string>(subscriber => {
     console.log('Observable executed');
     subscriber.next('Alice');
@@ -12,7 +13,15 @@ setTimeout(() => {
     console.log('Subscription 2 starts');
     observable$.subscribe((value: string) => console.log('Subscription 2:', value));
 }, 1000);
-const data$ = fromFetch('https://webapi-695u.onrender.com/foods').pipe(
+type WeightType = [{
+    _id: string;
+    __v: number;
+    date: string;
+    weight: number;
+    body_mass_index: number;
+    body_fat_percentage: number;
+}];
+const data$ = fromFetch('https://webapi-695u.onrender.com/weights').pipe(
     switchMap(response => {
         if (response.ok) {
             return response.json();
@@ -24,7 +33,37 @@ const data$ = fromFetch('https://webapi-695u.onrender.com/foods').pipe(
         console.error(err);
         return of({ error: true, message: err.message })
     })
-);
+)
 data$.subscribe({
-    next: result => console.log(result)
+    next: (result: WeightType) => {
+        const date = result.map(m => m.date);
+        const weight = result.map(m => m.weight);
+        const body_mass_index = result.map(m => m.body_mass_index);
+        const body_fat_percentage = result.map(m => m.body_fat_percentage);
+        const weights = {
+            labels: date,
+            datasets:
+            [
+                {
+                    label: 'Weight',
+                    data: weight,
+                    borderColor: 'rgb(75, 192, 192)'
+                },
+                {
+                    label: 'BMI',
+                    data: body_mass_index,
+                    borderColor: 'rgb(255, 99, 132)',
+                },
+                {
+                    label: 'Body Fat Percentage',
+                    data: body_fat_percentage,
+                    borderColor: 'rgb(54, 162, 235)',
+                }
+            ]
+        };
+        new Chart(document.getElementById('weight'), {
+            type: 'line',
+            data: weights
+        });
+    }
 });
