725d1696875a9a4340c513f4098d7892ff97a618
masa <dayoujianghong@gmail.com>
2022-09-29 13:55:32 +0900

    edit src/app.ts

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -2,6 +2,21 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./node_modules/rxjs/dist/cjs/fetch/index.js":
+/*!***************************************************!*\
+  !*** ./node_modules/rxjs/dist/cjs/fetch/index.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.fromFetch = void 0;
+var fetch_1 = __webpack_require__(/*! ../internal/observable/dom/fetch */ "./node_modules/rxjs/dist/cjs/internal/observable/dom/fetch.js");
+Object.defineProperty(exports, "fromFetch", ({ enumerable: true, get: function () { return fetch_1.fromFetch; } }));
+//# sourceMappingURL=index.js.map
+
+/***/ }),
+
 /***/ "./node_modules/rxjs/dist/cjs/index.js":
 /*!*********************************************!*\
   !*** ./node_modules/rxjs/dist/cjs/index.js ***!
@@ -1978,6 +1993,94 @@ function animationFramesFactory(timestampProvider) {
 }
 var DEFAULT_ANIMATION_FRAMES = animationFramesFactory();
 //# sourceMappingURL=animationFrames.js.map
+
+/***/ }),
+
+/***/ "./node_modules/rxjs/dist/cjs/internal/observable/dom/fetch.js":
+/*!*********************************************************************!*\
+  !*** ./node_modules/rxjs/dist/cjs/internal/observable/dom/fetch.js ***!
+  \*********************************************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.fromFetch = void 0;
+var OperatorSubscriber_1 = __webpack_require__(/*! ../../operators/OperatorSubscriber */ "./node_modules/rxjs/dist/cjs/internal/operators/OperatorSubscriber.js");
+var Observable_1 = __webpack_require__(/*! ../../Observable */ "./node_modules/rxjs/dist/cjs/internal/Observable.js");
+var innerFrom_1 = __webpack_require__(/*! ../../observable/innerFrom */ "./node_modules/rxjs/dist/cjs/internal/observable/innerFrom.js");
+function fromFetch(input, initWithSelector) {
+    if (initWithSelector === void 0) { initWithSelector = {}; }
+    var selector = initWithSelector.selector, init = __rest(initWithSelector, ["selector"]);
+    return new Observable_1.Observable(function (subscriber) {
+        var controller = new AbortController();
+        var signal = controller.signal;
+        var abortable = true;
+        var outerSignal = init.signal;
+        if (outerSignal) {
+            if (outerSignal.aborted) {
+                controller.abort();
+            }
+            else {
+                var outerSignalHandler_1 = function () {
+                    if (!signal.aborted) {
+                        controller.abort();
+                    }
+                };
+                outerSignal.addEventListener('abort', outerSignalHandler_1);
+                subscriber.add(function () { return outerSignal.removeEventListener('abort', outerSignalHandler_1); });
+            }
+        }
+        var perSubscriberInit = __assign(__assign({}, init), { signal: signal });
+        var handleError = function (err) {
+            abortable = false;
+            subscriber.error(err);
+        };
+        fetch(input, perSubscriberInit)
+            .then(function (response) {
+            if (selector) {
+                innerFrom_1.innerFrom(selector(response)).subscribe(OperatorSubscriber_1.createOperatorSubscriber(subscriber, undefined, function () {
+                    abortable = false;
+                    subscriber.complete();
+                }, handleError));
+            }
+            else {
+                abortable = false;
+                subscriber.next(response);
+                subscriber.complete();
+            }
+        })
+            .catch(handleError);
+        return function () {
+            if (abortable) {
+                controller.abort();
+            }
+        };
+    });
+}
+exports.fromFetch = fromFetch;
+//# sourceMappingURL=fetch.js.map
 
 /***/ }),
 
@@ -9458,6 +9561,7 @@ var exports = __webpack_exports__;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const rxjs_1 = __webpack_require__(/*! rxjs */ "./node_modules/rxjs/dist/cjs/index.js");
+const fetch_1 = __webpack_require__(/*! rxjs/fetch */ "./node_modules/rxjs/dist/cjs/fetch/index.js");
 const observable$ = new rxjs_1.Observable(subscriber => {
     console.log('Observable executed');
     subscriber.next('Alice');
@@ -9470,6 +9574,20 @@ setTimeout(() => {
     console.log('Subscription 2 starts');
     observable$.subscribe((value) => console.log('Subscription 2:', value));
 }, 1000);
+const data$ = (0, fetch_1.fromFetch)('https://webapi-695u.onrender.com/foods').pipe((0, rxjs_1.switchMap)(response => {
+    if (response.ok) {
+        return response.json();
+    }
+    else {
+        return (0, rxjs_1.of)({ error: true, message: `Error ${response.status}` });
+    }
+}), (0, rxjs_1.catchError)(err => {
+    console.error(err);
+    return (0, rxjs_1.of)({ error: true, message: err.message });
+}));
+data$.subscribe({
+    next: result => console.log(result)
+});
 
 })();
 
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs'
+import { Observable, switchMap, of, catchError } from 'rxjs'
+import { fromFetch } from 'rxjs/fetch';
 const observable$ = new Observable<string>(subscriber => {
     console.log('Observable executed');
     subscriber.next('Alice');
@@ -11,3 +12,19 @@ setTimeout(() => {
     console.log('Subscription 2 starts');
     observable$.subscribe((value: string) => console.log('Subscription 2:', value));
 }, 1000);
+const data$ = fromFetch('https://webapi-695u.onrender.com/foods').pipe(
+    switchMap(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return of({ error: true, message: `Error ${ response.status }` });
+        }
+    }),
+    catchError(err => {
+        console.error(err);
+        return of({ error: true, message: err.message })
+    })
+);
+data$.subscribe({
+    next: result => console.log(result)
+});
